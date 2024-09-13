d12cd2ced0aa528c8a52b98772cf22f4b977caf2
Yinchu <yinchux@andrew.cmu.edu>
2024-09-13 11:12:36 -0400

    f

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,58 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    private SortedIntList sortedIntList;
+    private int totalAdded;
+    public DelegationSortedIntList() {
+        // Initialize the delegated SortedIntList instance
+        sortedIntList = new SortedIntList();
+        totalAdded = 0;
+    }
+    @Override
+    public boolean add(int num) {
+        // Delegate to the SortedIntList's add method
+        boolean result = sortedIntList.add(num);
 
+        // If the element was successfully added, increment totalAdded
+        if (result) {
+            totalAdded++;
+        }
+
+        return result;
+    }
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean result = false;
+
+        // Iterate through the given list and delegate adding to sortedIntList
+        for (int i = 0; i < list.size(); i++) {
+            result |= this.add(list.get(i)); // Calls the add method, which tracks totalAdded
+        }
+
+        return result;
+    }
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+    @Override
+    public int get(int index) {
+        return sortedIntList.get(index); // Delegates to the internal SortedIntList
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return sortedIntList.remove(num); // Delegates to the internal SortedIntList
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedIntList.removeAll(list); // Delegates to the internal SortedIntList
+    }
+
+    @Override
+    public int size() {
+        return sortedIntList.size(); // Delegates to the internal SortedIntList
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,28 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
+    private int totalAdded = 0;
+    @Override
+    public boolean add(int num) {
+        boolean result = super.add(num);
+        if (result) {
+            totalAdded++;
+        }
+        return result;
+    }
 
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean result = false;
+        for (int i = 0; i < list.size(); i++) {
+            result |= add(list.get(i));
+        }
+        return result;
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
